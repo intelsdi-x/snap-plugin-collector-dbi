7244d90736e8f317e8b74b27cf4a2af97cd36ae3
Tiffany Jernigan <tiffany.jernigan@intel.com>
2016-05-05 23:52:05 -0700

    New metric schema

--- dbi/mock/mockData.go
+++ dbi/mock/mockData.go
@@ -20,32 +20,33 @@ import (
 	"time"
 
 	"github.com/intelsdi-x/snap/control/plugin"
+	"github.com/intelsdi-x/snap/core"
 )
 
 var (
 
 	// Mts is a mocked metrics
-	Mts = []plugin.PluginMetricType{
+	Mts = []plugin.MetricType{
 
 		// 1) For db = dbName1 one query=q1 is executed which has no additional info (like name or prefix)
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName1", "categoryA"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName1", "categoryB"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName1", "categoryC"}},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName1", "categoryA")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName1", "categoryB")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName1", "categoryC")},
 
 		// 2) For db = dbName2 two queries are executed:
 		// a) query=q1 - has no additional info (like name or prefix)
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "categoryA"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "categoryB"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "categoryC"}},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "categoryA")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "categoryB")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "categoryC")},
 
 		// b) query=q2 - has defined additional info (like resultName or instancePrefix) which are appended to a namespace
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "rName1", "category_prefix", "categoryA"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "rName1", "category_prefix", "categoryB"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "rName1", "category_prefix", "categoryC"}},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "rName1", "category_prefix", "categoryA")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "rName1", "category_prefix", "categoryB")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "rName1", "category_prefix", "categoryC")},
 
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "rName2", "categoryA"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "rName2", "categoryB"}},
-		plugin.PluginMetricType{Namespace_: []string{"intel", "dbi", "dbName2", "rName2", "categoryC"}},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "rName2", "categoryA")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "rName2", "categoryB")},
+		plugin.MetricType{Namespace_: core.NewNamespace("intel", "dbi", "dbName2", "rName2", "categoryC")},
 	}
 
 	// QueryOutput is a mocked query output
